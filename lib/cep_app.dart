b048846d304cc872c1a0a935a7a690a0668d0581
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-12-18 21:56:17 -0300

    feat: clean init app and config theme

---
+++ lib/cep_app.dart
@@ -0,0 +1,27 @@
+import 'package:cep_flutter/pages/main_page.dart';
+import 'package:flutter/material.dart';
+
+class CepApp extends StatelessWidget {
+  const CepApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CEP App',
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          surface: Colors.green,
+        ),
+        useMaterial3: true,
+      ),
+      home: const MainPage(title: 'CEP App'),
+    );
+  }
+}
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'To do',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
